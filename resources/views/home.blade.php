6c1689512fefdce3bf18ac1f0a197e93af847b60
MaxPredathor <max.gilli98@gmail.com>
2024-01-19 20:02:04 +0100

    added pie chart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,26 @@
 @extends('layouts.app')
 @section('content')
     <section class="container">
-        <canvas id="myChart"></canvas>
+        <canvas class="my-4" id="myChart"></canvas>
+        <canvas id="myPieChart"></canvas>
         <script>
             const ctx = document.getElementById('myChart');
 
             new Chart(ctx, {
                 type: 'bar',
                 data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    labels: ['HTML', 'SASS', 'Vue', 'PHP', 'Laravel', 'Bootstrap'],
                     datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        borderWidth: 1
-                    }]
+                            label: '# of Projects',
+                            data: [12, 19, 3, 5, 2, 3],
+                            borderWidth: 1
+                        },
+                        {
+                            label: '# of Test Projects',
+                            data: [3, 10, 4, 2, 7, 12],
+                            borderWidth: 1
+                        }
+                    ]
                 },
                 options: {
                     scales: {
@@ -22,6 +29,20 @@
                         }
                     }
                 }
+            });
+        </script>
+        <script>
+            const myPieChart = document.getElementById('myPieChart');
+            new Chart(myPieChart, {
+                type: 'pie',
+                data: {
+                    labels: ['Vue', 'Laravel', 'Both'],
+                    datasets: [{
+                        data: [50, 30, 20],
+                        backgroundColor: ['green', 'purple', 'black'],
+                        hoverOffset: 4
+                    }]
+                },
             });
         </script>
     </section>
